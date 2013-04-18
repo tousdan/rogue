d19a1174729e8be43bc912dbaa1476a826a280a5
tousdan <tousdan@gmail.com>
2013-04-17 21:22:31 -0400

    Huge first commit

---
+++ Rogue-desktop/src/gdt/DesktopGame.java
@@ -0,0 +1,17 @@
+package gdt;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class DesktopGame {
+	public static void main (String[] args) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.title = "Rogue";
+		config.useGL20 = false;
+		config.width = 480;
+		config.height = 320;
+		
+		new LwjglApplication(new Game(), config);
+	}
+}
