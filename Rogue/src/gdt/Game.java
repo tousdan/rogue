290eafcd7ef0ff59d9d5ccd7e7612cd2acaa8d07
tousdan <tousdan@gmail.com>
2013-04-18 23:04:10 -0400

    yaxis down

--- Rogue/src/gdt/Game.java
+++ Rogue/src/gdt/Game.java
@@ -13,10 +13,8 @@ public class Game implements ApplicationListener {
 	public static final int SCREEN_WIDTH = 1024;
 	public static final int SCREEN_HEIGHT = 768;
 	
-	public static final int CAMERA_WIDTH = SCREEN_WIDTH;
-	public static final int CAMERA_HEIGHT = SCREEN_HEIGHT;
-	
-	
+	public static final int CAMERA_WIDTH = 640;
+	public static final int CAMERA_HEIGHT = 460;
 	
 	private static final int LEVEL_WIDTH = 256;
 	private static final int LEVEL_HEIGHT = 256;
@@ -37,10 +35,10 @@ public class Game implements ApplicationListener {
 		
 		frostshock = new Texture(Gdx.files.internal("assets/frostshock.png"));
 		
-		frostshock_loc = new Rectangle(CAMERA_WIDTH / 2 , CAMERA_HEIGHT / 2, 64, 64);
+		frostshock_loc = new Rectangle(10, 10, 64, 64);
 		
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, CAMERA_WIDTH, CAMERA_HEIGHT);
+		camera.setToOrtho(true, CAMERA_WIDTH, CAMERA_HEIGHT);
 		
 		batch = new SpriteBatch();
 	}
@@ -56,7 +54,7 @@ public class Game implements ApplicationListener {
 	public void render() {
 		handleInput();
 		handleCameraInput();
-		centerCamera();
+		
 		
 		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
@@ -77,14 +75,13 @@ public class Game implements ApplicationListener {
 				int x = i % LEVEL_WIDTH * 8;
 				int y = i / LEVEL_WIDTH * 8;
 				
-				if(y > 0) {
-					++y;
-				}
+			
 				batch.draw(tex, x, y);
 			}
 		}
 	    batch.draw(frostshock, frostshock_loc.x, frostshock_loc.y);
 	    batch.end();
+	    
 	}
 
 	@Override
@@ -113,11 +110,11 @@ public class Game implements ApplicationListener {
 			frostshock_loc.x += 5;
 		} 
 		
-		if(Gdx.input.isKeyPressed(Keys.UP)) {
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
 			frostshock_loc.y += 5;
 		} 
 		
-		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
 			frostshock_loc.y -= 5;
 		}
 		
@@ -132,24 +129,6 @@ public class Game implements ApplicationListener {
 		} else if(frostshock_loc.y >= SCREEN_HEIGHT - frostshock_loc.height) {
 			frostshock_loc.y = SCREEN_HEIGHT - frostshock_loc.height;
 		}
-	}
-	
-	private void centerCamera() {
-		float new_x = frostshock_loc.x;
-		if(new_x < CAMERA_WIDTH / 2) {
-			new_x = CAMERA_WIDTH / 2;
-		} else if(new_x > SCREEN_WIDTH - CAMERA_WIDTH / 2) {
-			new_x = SCREEN_WIDTH - CAMERA_WIDTH / 2;
-		}
-		
-		float new_y = frostshock_loc.y;
-		if(new_y < CAMERA_HEIGHT / 2) {
-			new_y = CAMERA_HEIGHT / 2;
-		} else if(new_y > SCREEN_HEIGHT - CAMERA_HEIGHT / 2) {
-			new_y = SCREEN_HEIGHT - CAMERA_HEIGHT / 2;
-		}
-		
-		camera.position.set(new_x, new_y, 1); 
 	}
 	
 	private void handleCameraInput() {
